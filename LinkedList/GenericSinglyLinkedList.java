7c27645e5bde494e28af58614c3c1fd4b22f0665
Bhavesh Nafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-03-23 18:34:33 +0530

    Initial commit

---
+++ LinkedList/GenericSinglyLinkedList.java
@@ -0,0 +1,178 @@
+import java.util.*;
+class MySinglyLinkedList<E>{
+
+  MyNode<E> head;
+
+  void add(E data){
+    MyNode<E> toAdd = new MyNode<E>(data);
+    if(isEmpty()){
+      head = toAdd;
+      return;
+    }
+    MyNode<E> temp = head;
+    while(temp.next != null){
+      temp = temp.next;
+    }
+    temp.next = toAdd;
+  }
+
+  void print(){
+    MyNode<E> temp = head;
+    if(isEmpty()){
+      System.out.println("List is Empty!");
+      return;
+    }
+    while(temp != null){
+      System.out.print(temp.data+" ");
+      temp = temp.next;
+    }
+  }
+
+  E get(int index){
+    if(isEmpty()){
+      System.out.println("List is Empty!");
+      return null;
+    }else if(size()<index){
+      System.out.println("index "+index+" is unreachable");
+      return null;
+    }
+    MyNode<E> temp = head;
+    while(index-->0){
+      temp = temp.next;
+
+    }
+    return temp.data;
+  }
+
+  void put(int index, E value){
+    if(isEmpty()){
+      System.out.println("List is Empty!");
+      return ;
+    }else if(size()<index){
+      System.out.println("index "+index+" is unreachable");
+      return;
+    }
+    MyNode<E> temp = head;
+    while(index-->0){
+      temp = temp.next;
+
+    }
+    temp.data = value;
+  }
+
+  int size(){
+    if(isEmpty()){
+      return 0;
+    }
+    MyNode<E> temp = head;
+    int size = 0;
+    while(temp != null){
+      size++;
+      temp = temp.next;
+    }
+    return size;
+  }
+
+  boolean isEmpty(){
+    return head == null;
+  }
+
+  void clear(){
+    if(isEmpty()){
+      System.out.println("List is Empty!");
+      return;
+    }
+    head = null;
+  }
+
+  void remove(int index){
+    if(isEmpty()){
+      System.out.println("List is Empty!");
+      return;
+    }else if(size()<index){
+      System.out.println("index "+index+" is unreachable");
+      return;
+    }
+    MyNode<E> temp = head;
+    MyNode<E> toRemove;
+    if(index == 0){
+      head = head.next;
+      return;
+    }
+    while(index-->0){
+      if(index == 0){
+        toRemove = temp.next;
+        temp.next = toRemove.next;
+        return;
+      }else{
+        temp = temp.next;
+      }
+    }
+
+  }
+
+  static class MyNode<E>{
+    E data;
+    MyNode<E> next;
+    MyNode(E data){
+      this.data = data;
+    }
+  }
+}
+
+public class GenericSinglyLinkedList{
+  public static void main(String bhavesh[]){
+    Scanner sc = new Scanner(System.in);
+    MySinglyLinkedList<Integer> msll = new MySinglyLinkedList<>();
+    int choice = -1;
+    System.out.println("1.> Add");
+    System.out.println("2.> Get");
+    System.out.println("3.> Print");
+    System.out.println("4.> Put");
+    System.out.println("5.> Remove");
+    System.out.println("6.> clear");
+    System.out.println("7.> RemoveAll");
+    System.out.println("8.> Size");
+    System.out.println("9.> EXIT");
+    while(choice != 9){
+      System.out.print("ENTER YOUR CHOICE : ");
+      choice  = sc.nextInt();
+      switch(choice){
+        case 1 :  System.out.println("Enter Element to be added") ;
+                  int ele = sc.nextInt();
+                  msll.add(ele);
+                  break;
+        case 2 :  System.out.println("Enter index of Element to be fetched") ;
+                  int idx = sc.nextInt();
+                  System.out.println("Element at index "+idx+" is : "+msll.get(idx)) ;
+                  break;
+        case 3 :  msll.print();
+                  System.out.println();
+                  break;
+        case 4 :  System.out.println("Enter index of Element to be inserted and element") ;
+                  idx = sc.nextInt();
+                  ele = sc.nextInt();
+                  msll.put(idx,ele);
+                  msll.print();
+                  break;
+        case 5 :  System.out.println("Enter index of Element to be removed") ;
+                  idx = sc.nextInt();
+                  msll.remove(idx);
+                  msll.print();
+                  break;
+        case 6 :  msll.clear();
+                  break;
+        case 7 :  System.out.println("Enter index of Element to be fetched") ;
+
+                  break;
+        case 8 :  System.out.println("Size : "+msll.size()) ;
+
+                  break;
+        case 9 :  System.out.println("EXITING");
+                  break;
+        default : System.out.println("Wrong Choice! Enter Again") ;
+
+      }
+    }
+  }
+}
